2264c870592e36e46babe0efd85abe49f47b8dd9
rkrug5 <phishermankrug@hotmail.com>
2018-06-19 21:51:37 -0700

    styling

--- react-ui/src/components/SearchForm/SearchForm.js
+++ react-ui/src/components/SearchForm/SearchForm.js
@@ -4,20 +4,28 @@ import "./SearchForm.css";
 const SearchForm = props => (
 
 
-<form>
-<div class="form-group">
-  <label for="exampleFormControlInput1">Message Title</label>
-  <input type="title" class="form-control" id="exampleFormControlInput1" placeholder="Title"/>
-</div>
+  <form >
+    <div className="container">
+      <div className="form-group row">
+        <div className="col-5 ">
+          <label for="exampleFormControlInput1">Message Title</label>
+          <input type="title" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
+        </div>
+      </div>
 
 
+      <div className="form-group">
+        <div className="form-group row">
+          <div className="col-5">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary">Toss it into the sea!</button>
+    </div >
 
-<div class="form-group">
-  <label for="exampleFormControlTextarea1">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-</div>
-<button type="submit" class="btn btn-primary">Toss it into the sea!</button>
-</form>
+  </form >
 );
 
 export default SearchForm;
